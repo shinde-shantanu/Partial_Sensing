fea2d71d1ef66a6a408a8775875c60ff892b71d5
shinde-shantanu <shantanushinde.shinde@gmail.com>
2021-10-16 18:10:04 -0400

    Agent 4 Re-Factoring

--- Agent_4.py
+++ Agent_4.py
@@ -1,20 +1,7 @@
-from Grid_Generator import gen_grid
 from A_Star import a_star
 import timeit
-import pandas as pd
 
-def test(C, B, E, H, N, dim, visited):
-    for i in range(dim):
-        for j in range(dim):
-            #if visited[i][j] != 1:
-                #continue
-            if (B[i][j] + E[i][j] + H[i][j]) != N[i][j]:
-                print("ERROR ERROR ERROR", i, j, B[i][j], E[i][j], H[i][j], N[i][j])
-            if B[i][j] > sense(i,j,grid):#C[i][j]:
-                print("ERROR 2 ERROR 2 ERROR 2", i, j, B[i][j], E[i][j], H[i][j], N[i][j], sense(i,j,grid))
-    print("DONE DONE DONE")
-
-def sense(i, j, grid):
+def sense(i, j, grid, dim):
     ans=0
     for I in [i-1,i,i+1]:
         for J in [j-1,j,j+1]:
@@ -96,6 +83,14 @@ def check_path(path, dis):
             return False
     return True
 
+def con_vis(con, vis, dim):
+    c = 0
+    for i in range(dim):
+        for j in range(dim):
+            if con[i][j] != -1:
+                c = c + 1
+    return c-vis
+
 def agent_4(grid, dim, P, heu):
     
     start = timeit.default_timer() #recording time stamp to measure run time
@@ -107,7 +102,7 @@ def agent_4(grid, dim, P, heu):
     con[dim-1][dim-1] = 0
 
     s = [[0 for i in range(dim)] for j in range(dim)]
-    s[0][0] = sense(0,0,grid)
+    s[0][0] = sense(0,0,grid,dim)
     
     rules = []
     
@@ -121,40 +116,47 @@ def agent_4(grid, dim, P, heu):
     
     final = [] #Data structure to store final trajectory
     
-    cells = 0
+    vis = 0
+    plan_t = 0
+    bumps = 0
 
     add_rule(0, 0, s[0][0], con, rules, rec, dim)
     c_in = inference(rules, con, dis, rec)
     
     while done != True:
+
+        ps = timeit.default_timer()
         result, parent =a_star(dim, P, dis, heu, si, sj) #planning stage of repeated A*
+        plan_t = plan_t + (timeit.default_timer() - ps)
+        
         if result == False: #true if grid not solvable
             break
         
         path = find_path(parent, dim, si, sj)
-        cells = cells + search_size(parent, dim) #used to record total number of cells processed
-        print("Path: ", path)
+        
         flag = True
         for (i, j) in path: #agent traversing the planned path
-            print((i,j))
+
+            vis = vis + 1
+
             con[i][j] = grid[i][j]
             dis[i][j] = con[i][j]
             update_rules(i, j, con[i][j], rec[i][j], rules)
             rec[i][j]=[]
             if grid[i][j] == 1: #only updating the grid knowledge after agent bumps into a blocked node
+                bumps = bumps + 1
                 (si, sj) = parent[i][j]
                 final.pop(len(final)-1)
                 flag = False
                 break
             else:
-                s[i][j] = sense(i,j,grid)
+                s[i][j] = sense(i,j,grid,dim)
                 add_rule(i, j, s[i][j], con, rules, rec, dim)
             c_in = inference(rules, con, dis, rec)
             if c_in != 0:
                 if not check_path(path, dis):
                     flag=False
                     (si, sj) = (i,j)
-                    #final.pop(len(final)-1)
                     break
             final.append((i, j))
 
@@ -162,23 +164,5 @@ def agent_4(grid, dim, P, heu):
             done = True
     
     stop = timeit.default_timer()
-
-    print("con")
-    for x in con:
-        print(x)
-
-    return(result, final, dis, cells, start, stop) #recording time stamp to measure run time
-
-dim = 5
-p = 0.2
-grid = gen_grid(dim, p) #generating grid
-#grid = [[0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 1, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0]]
-#grid = [[0, 0, 1], [1, 0, 0], [0, 0, 0]]
-#grid = [[0, 0, 0], [0, 0, 1], [0, 0, 0]]
-#grid = [[0, 0, 0], [0, 1, 1], [0, 0, 0]]
-print(grid)
-for x in grid:
-    print(x)
-result, traj, dis, cells_processed, start, stop = agent_4(grid, dim, p, 2)
-print("Result: ",result)
-print("Trajectory: ",traj)
+    
+    return(result, final, dis, vis, start, stop, plan_t, bumps, con_vis(con, vis, dim)) #recording time stamp to measure run time
